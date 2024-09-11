2b4b9c48c849393fadeaca345e116c530d164c38
KeineSchere <125075466+KeineSchere@users.noreply.github.com>
2024-09-11 21:17:01 +0200

    Final Version

--- #Synch with File Server.py
+++ #Synch with File Server.py
@@ -1,6 +1,6 @@
-#Synch with File Server
-#This script synchronizes two partions or directorys
-#Author: Fabian Weiershausen
+#Synch with file server
+#This script synchronizes two directorys or whatever you want
+#Author: KeineSchere
 #License: GPLv3 // https://www.gnu.org/licenses/gpl-3.0.de.html
 from IPython.display import display
 import os
@@ -9,17 +9,22 @@ import time
 import shutil
 from datetime import datetime
 
-ServerDF = pd.DataFrame(columns=['Name', 'Created2', 'Modified2', 'Paath2', 'Parent2'])
+#Replace with your directory path you want to copy
+Synch_From = r'D:/ExampleCopyFrom/'
+#Replace with your directory path you want to copy to
+Synch_To   = r'Y:/ExampleCopyTo/'
+#Replace with the location you want to safe the log
+Safe_Log   = r'Y:ExampleSafeLocation/'
+
+ServerDF = pd.DataFrame(columns=['Name', 'Created2', 'Modified2', 'Path2', 'Parent2'])
 NameSe = 'ServerDF'
 WinDF = pd.DataFrame(columns=['Name', 'Created', 'Modified', 'Path', 'Parent', 'Modded'])
 NameWIN = 'WinDF'
 LogDF = pd.DataFrame(columns=['Action, Time, Info'])
 CountServer = 0
 CountWin = 0
-Safe_Log = r'X:/Synch/Log/'
 
 def Get_Data(dir_path, DF, count, Name):
-       
     for path in os.listdir(dir_path):
            
         print(dir_path)
@@ -32,7 +37,7 @@ def Get_Data(dir_path, DF, count, Name):
             LogDF._append({'Action':'Cancel Access', 
                             'Time': time, 
                             'Info': 'Windows Trash'}, ignore_index=True)
-            print('Fick dich Windows!!!')
+            print('Fuck Windows!')
                     
         else: 
         
@@ -60,67 +65,53 @@ def Get_Data(dir_path, DF, count, Name):
                 File_Path = File_Path + '/'
                 Get_Data(File_Path, DF, count, Name)
         
-                display(DF)
                 print(f"{count} Objects were Counted in Directory")
-
+    
 def Set_Flag(MainDF):
+    print("\nStart of Set_Flag")
     for ind in range(len(MainDF)):
-        # File Changed 
+        #File changed
         try:
-            #Chech if Obj is a Directory if so, no need to replace 
+            #Check if is a file to update
             if os.path.isfile(MainDF.loc[ind, "Path"]):
-                if (MainDF.loc[ind, "Modified"]) != (MainDF.loc[ind, 'Modified2']):
-                    print('checkM')
+                if (MainDF.loc[ind, "Modified"]) >= (MainDF.loc[ind, 'Modified2']):
                     MainDF.loc[ind, "Modded"] = 'M'
-                    print (MainDF.Modded[ind])
-            
-                # New Path
-                elif (MainDF.loc[ind, "Path"]) != (MainDF.loc[ind, 'Path2']):
-                    print('checkP')
-                    MainDF.loc[ind, "Modded"] = 'P'
-                    print (MainDF.Modded[ind])
-            
-                # New File
-                elif (MainDF.loc[ind, "Name"]) != (MainDF.loc[ind, 'Name2']):
-                    print('checkNF')
-                    MainDF.loc[ind, "Modded"] = 'NF'
-                    print (MainDF.Modded[ind])
-            else:
-                print('Not a File')
-                
+                    print ("Found modded: ",MainDF.Name[ind])
         except:
             print("Catched")        
+    print("End of Set_Flag\n")
 
 def Check_Change(MainDF):
-     for Modified in MainDF.index:
-        try:
-            #Whole Path for Controll that it is really a file and not a Directory 
+    print("\nStart of Check_Changed")
+    for Modified in MainDF.index:
+        try: 
             if (MainDF['Modded'][Modified] == 'M'):
-                #Change File with new Directory or Del File and replace with new Version
+                #Delete old file and uploade new file
                 print("Try Modifying")
 
                 Source_Path = (MainDF['Path'][Modified])
                 print(Source_Path)
                 Dest_Path   = (MainDF['Parent2'][Modified])
                 
-                #LogDF._append({'Action':'Updated File', 
-                #               'Time': time, 
-                #               'Info': (MainDF['Path'][Modified])}, ignore_index=True)
+                LogDF._append({'Action':'Updated File', 
+                               'Time': time, 
+                               'Info': (MainDF['Path'][Modified])}, ignore_index=True)
                    
                 #Delte modified file and copy new version of the file
-                print("test")
                 Del_File = os.path.join(MainDF['Parent2'][Modified],MainDF['Name'][Modified])
                 os.remove(Del_File)
-                print ("removed")
+                print ("Removed old Version")
                 shutil.copy(str(Source_Path), str(Dest_Path))
-            
+                print ("Succesfully uploaded new Version")
         except:
             print("Catched")
             LogDF._append({'Action':'Update File ERROR', 
                             'Time': time, 
                             'Info': (WinDF['Name'][Modified])}, ignore_index=True)
-                
+    print("End of Check_Changed\n")
+    
 def Create_Log(MainDF, LogDF):
+    print("\nStart of creating Log")
     now = datetime.now()
     current_time = now.strftime("%Y-%m-%d_%H-%M-%S")
     Safe_Path = Safe_Log + "Log" + str(current_time)
@@ -133,23 +124,107 @@ def Create_Log(MainDF, LogDF):
     #    WinDF = WinDF.to_string(header=True, index=True)
     #    Write_Log = "Win:" ,WinDF, "Server:", ServerDF
     #    Log.write(str(Write_Log))
-
+    print("End of creating Log\n")
+    
+def Check_Exist(WinDF, ServerDF):
+    print("\nStart of check_exist")
+    for index, row in WinDF.iterrows():
+        try:
+            check = ""
+            Pa  = row.Path.replace(Synch_From, "")
+               
+            for index, row2 in ServerDF.iterrows():
+                try:
+                    Pa2 = row2.Path2.replace(Synch_To, "")
+                        
+                    if (row2.Name == row.Name and Pa2 == Pa):
+                        #Found the same dir on server / no synch requierd
+                        check = "X"
+                except:
+                    print("Error")
+                
+            #Create new file/dir
+            if (check == ""):
+                    
+                New_Path = Synch_To + Pa
+                    
+                print(New_Path)
+                    
+                if os.path.isdir(row.Path):
+                    print("Creating directory: ", New_Path)
+                    os.makedirs(New_Path)
+                        
+                    LogDF._append({'Action':'Created new dir', 
+                                     'Time': time, 
+                                     'Info': (WinDF['Name']["new dir"])}, ignore_index=True)
+                        
+                elif os.path.isfile(row.Path):
+                    print("Copying file: ", New_Path)
+                    shutil.copy(str(row.Path), str(New_Path))
+                    LogDF._append({'Action':'Copied new file', 
+                                     'Time': time, 
+                                     'Info': (WinDF['Name']["new file"])}, ignore_index=True)
+        except:
+            print("Error_Check_Exist")
+    print("End of check_exist\n")
+        
+def Junk_detection(WinDF, ServerDF):
+    print("\nStart of Junk_Detection")
+    for index, row2 in ServerDF.iterrows():
+        try:
+            Pa2 = row2.Path2.replace(Synch_To, "")
+            check = ""
+            for index, row in WinDF.iterrows():
+                if (row.Name == row2.Name):
+                    Pa = row.Path.replace(Synch_From, "")
+                    
+                    if (Pa2 == Pa):  
+                        check = 'X'
+                        break
+                    
+            if (check == ""):
+                local_path = r'%s' % row2.Path2
+                print("Removing: ", local_path)
+                
+                if os.path.isdir(local_path):
+                    print("Rm dir")
+                    os.rmdir(local_path)
+                    
+                elif os.path.isfile(local_path):
+                    print("Rm file")
+                    os.remove(local_path)
+        except:
+            print("Error while running junk detection")  
+    print("End of Junk_Detection\n")    
+    
+#-----------------------------------------------------------------
+#Start of script
 #Read client partion for synch with server
-Get_Data(r'D:/', WinDF, CountWin, NameWIN)
+Get_Data(Synch_From, WinDF, CountWin, NameWIN)
 #Read server partion for synch with client
-Get_Data(r'X:/Dateien/', ServerDF, CountServer, NameSe)
+Get_Data(Synch_To, ServerDF, CountServer, NameSe)
 
 WinDF.drop_duplicates()
 ServerDF.drop_duplicates()
 
+#Deletes asynch files and directorys
+Junk_detection(WinDF, ServerDF)
+
+#Check if corrosponding file and directory is destination side
+# and uploade/creat new directory or files
+Check_Exist(WinDF, ServerDF)
+
 MainDF = pd.merge(WinDF, ServerDF, how="inner", on=["Name"])
 MainDF.drop_duplicates()
-display(MainDF)
 
+#Modifying files that exists on both systems in the same directory
 Set_Flag(MainDF)
 
 Check_Change(MainDF)
 
+#if you dont need logs make the following line to a comment
 Create_Log(MainDF, LogDF)
 
 print("Finished")
+#End of script
+#-----------------------------------------------------------------
